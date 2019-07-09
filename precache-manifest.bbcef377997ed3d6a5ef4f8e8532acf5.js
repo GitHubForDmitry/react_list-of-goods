5f4d0c0baa8c8fb50ddf09a4586eacce4532d3cd
GitHubForDmitry <dmitrykoka@gmail.com>
2019-07-09 23:18:49 +0300

    Updates

---
+++ precache-manifest.bbcef377997ed3d6a5ef4f8e8532acf5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd30f494bc6ace6ceb1d2ac6cc8a34f3",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "ade9938523140266cf92",
+    "url": "/react_list-of-goods/static/css/main.80e65e5f.chunk.css"
+  },
+  {
+    "revision": "af634582563219ba7405",
+    "url": "/react_list-of-goods/static/js/2.e60fca39.chunk.js"
+  },
+  {
+    "revision": "ade9938523140266cf92",
+    "url": "/react_list-of-goods/static/js/main.cd77866d.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
