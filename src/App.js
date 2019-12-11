39a4d9e93cebc4d5b54bf13cfdf0c16545a93c2a
Дмитрий Кока <dmitrykoka@gmail.com>
2019-12-11 23:59:13 +0200

    hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import goodsFromServer from './data/goods';
 
-const goodsFromServer = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
-];
+const App = () => {
+  const [goods, setGoods] = useState([...goodsFromServer]);
+  const [loadGoods, setLoadGoods] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [wordLength, setwordLength] = useState(1);
+  const [direction, setDirection] = useState(1);
 
-const App = () => (
-  <div className="App">
-    <h1>Goods 1</h1>
-  </div>
-);
+  const loadData = () => {
+    setTimeout(() => {
+      setLoadGoods(prevState => true);
+      setIsLoading(prevState => false);
+    }, 500);
+    setIsLoading(true);
+  };
+  const reverse = () => {
+    const sorted = [...goods].reverse();
+
+    setGoods(sorted);
+  };
+  const sortAlphabetically = () => {
+    const sorted = [...goods].sort((a, b) => a !== b ? a < b ? -1 : 1 : 0);
+
+    setGoods([...sorted]);
+  };
+
+  if (!loadGoods) {
+    return (
+      <button
+        type="submit"
+        className="btn btn-success mt-5"
+        onClick={() => loadData()}
+        disabled={isLoading}
+      >
+        {isLoading ? '...Loading' : 'Load'}
+      </button>
+    );
+  }
+
+  return (
+    <div className="goods">
+      {goods
+        .map(good => <li key={good.toString()}>{good}</li>)}
+      <button
+        type="submit"
+        className="btn btn-primary mt-5 mb-5 mr-3"
+        onClick={reverse}
+      >
+        reverse
+      </button>
+      <button
+        type="submit"
+        className="btn btn-primary mt-5 mb-5 mr-3"
+        onClick={sortAlphabetically}
+      >
+        getSortAlphabetically
+      </button>
+    </div>
+  );
+};
 
 export default App;
